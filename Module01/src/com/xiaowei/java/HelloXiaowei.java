06c5c79d66caf65f8741bc8d6c72b5ac3dc05b56
xiaoweichaoxinxing <1548955696@qq.com>
2019-04-18 17:22:16 +0800

    first commit

---
+++ Module01/src/com/xiaowei/java/HelloXiaowei.java
@@ -0,0 +1,13 @@
+package com.xiaowei.java;
+
+/**
+ * @author xiaowei
+ * @create 2019/4/18 - 17:19
+ */
+public class HelloXiaowei {
+
+
+    public static void main(String[] args) {
+        System.out.println("xiaowei,nihao!");
+    }
+}
